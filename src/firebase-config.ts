34cd8c34d3f234993886bad454579ff7b16ee020
pandadamayanti01-stack <pandadamayanti01@gmail.com>
2026-04-15 07:17:47 +0530

    Fix storageBucket domain in firebase config.com
    Updated storageBucket URL to correct domain.

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -4,7 +4,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyB53Wdjm_c8-55XeBuyxj5l_ozRxfc6Pbc",
   authDomain: "utkalskillcentre.firebaseapp.com",
   firestoreDatabaseId: "ai-studio-2a24dfcb-5874-4b37-8e37-434f425283b9",
-  storageBucket: "utkalskillcentre.firebasestorage.app",
+  storageBucket: "utkalskillcentre.firebasestorage.com",
   messagingSenderId: "108736001473",
   measurementId: "G-Z2M2G8LL46"
 };
