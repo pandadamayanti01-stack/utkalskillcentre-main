4052a11df2bbae86e467537421232e9acb70fedf
Gyanalok Panda <pandadamayanti01@gmail.com>
2026-05-20 01:14:24 +0530

    UI: Update social_science translation to Social Science for classes 6-8

--- src/translations.ts
+++ src/translations.ts
@@ -185,7 +185,7 @@ export const translations: any = {
       epe: "Environmental & Population Education",
       aspirational: "Aspirational Components",
       science_curiosity: "Science (Jigyansa)",
-      social_science: "History"
+      social_science: "Social Science"
     },
     subjectsByBoard: {
       odisha: {
@@ -527,7 +527,7 @@ export const translations: any = {
       epe: "ପରିବେଶ ଏବଂ ଜନସଂଖ୍ୟା ଶିକ୍ଷା",
       aspirational: "ଆକାଂକ୍ଷାମୂଳକ ଉପାଦାନ",
       science_curiosity: "ବିଜ୍ଞାନ (ଜିଜ୍ଞାସା)",
-      social_science: "ଇତିହାସ"
+      social_science: "ସାମାଜିକ ବିଜ୍ଞାନ"
     },
     subjectsByBoard: {
       odisha: {
